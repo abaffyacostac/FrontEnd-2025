e4d56584b761a7358b856b03ef9f066aea093db7
abaffyacostac <abaffyacostac@student.douglascollege.ca>
2025-03-21 09:57:30 -0700

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_URL = 'https://mongobackend2025-6r43.onrender.com';
+
+export const getUsers = () => axios.get(`${API_URL}/users`);
+export const getUserById = (id) => axios.get(`${API_URL}/users/${id}`);
+export const createUser = (user) => axios.post(`${API_URL}/users`, user);
+export const updateUser = (id, user) => axios.put(`${API_URL}/users/${id}`, user);
+export const deleteUser = (id) => axios.delete(`${API_URL}/users/${id}`);
